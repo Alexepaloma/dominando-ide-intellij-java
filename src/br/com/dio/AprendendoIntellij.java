60707eb7dfcfdb092d43cb970b57db5e9e17eaa1
Alexander <alexaraujo2209@gmail.com>
2022-04-13 10:14:40 -0300

    second commit

---
+++ src/br/com/dio/AprendendoIntellij.java
@@ -0,0 +1,9 @@
+package br.com.dio;
+
+public class AprendendoIntellij {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 7;
+        System.out.println("Tudo bem galera? " + (a+b));
+    }
+}
